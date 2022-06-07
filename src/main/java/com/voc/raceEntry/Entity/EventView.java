26ffd1ff4624f12e9b8c28eae440b24d3f2a039b
adamgoodridge <adsmacmail@gmail.com>
2022-06-07 16:02:17 +1000

    Deleted Table's fields' annotations

--- src/main/java/com/voc/raceEntry/Entity/EventView.java
+++ src/main/java/com/voc/raceEntry/Entity/EventView.java
@@ -1,41 +1,28 @@
 package com.voc.raceEntry.Entity;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Entity
-@Table(name = "event_view")
 public class EventView {
 
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
-    @Column(name = "event_id")
     private Long eventId;
-    @Basic
-    @Column(name = "name")
     @NotNull(message = "Event's name is required.")
     private String name;
-    @Basic
-    @Column(name = "venue")
     @NotNull(message = "Event's venue is required.")
     private String venue;
-    @Basic
-    @Column(name = "date")
     @NotNull(message = "Event's date is required.")
     private String date;
-    @Basic
-    @Column(name = "open")
     private Integer open;
-    @Basic
-    @Column(name = "visible")
     private Integer visible;
-
-    @Column(name = "entries_count")
     private Integer entriesCount;
 
-    @OneToMany(cascade={CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
-    @JoinColumn(name = "event_id")
     private List<Entry> entries;
 
     public List<Entry> getEntries() {
@@ -99,21 +86,13 @@ public class EventView {
         String[] fields = {"name","venue","date"};
         String[] fieldsNames = {"Name","Venue","Date"};
         for(int i = 0; i < fields.length; i++) {
-            /*
-            sb.append("<div class=\"mb-3\">");
-            sb.append("\n<label for=\"" +fields[i] + "\" class=\"form-label\">Enter your "+ fieldsNames[i] + "</label>");
-            sb.append("\n<form:input path=\""+ fields[i] +"\" id=\""+ fields[i] +"\" class=\"form-control\" /><form:errors path=\""
-                    + fields[i] +"\" cssClass=\"error\">");
-            sb.append("\n</div>");
-
-             */
             sb.append("<div class=\"mb-3\">\n");
-            sb.append("<label class=\"form-label\">Event's "+ fieldsNames[i] +"</label>\n");
-            sb.append("<input type=\"text\" th:field=\"*{"+fields[i]+"}\" class=\"form-control\" >\n");
-            sb.append("<p th:if=\"${#fields.hasErrors('"+fields[i]+"\')}\" th:errorclass=\"error\" th:errors=\"*{"+fields[i]+"}\" />\n");
+            sb.append("<label class=\"form-label\">Event's " + fieldsNames[i] + "</label>\n");
+            sb.append("<input type=\"text\" th:field=\"*{" + fields[i] + "}\" class=\"form-control\" >\n");
+            sb.append("<p th:if=\"${#fields.hasErrors('" + fields[i] + "')}\" th:errorclass=\"error\" th:errors=\"*{" + fields[i] + "}\" />\n");
             sb.append("</div>\n");
         }
-        System.out.println(sb.toString());
+        System.out.println(sb);
     }
 
     public Integer getEntriesCount() {
