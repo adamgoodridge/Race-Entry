97cb792cf2fc6877cc72f827475ad93d626d7d5a
adamgoodridge <adsmacmail@gmail.com>
2023-04-07 12:58:04 +1000

    Added utilities classes & the library commons-text from Apache Commons Lang.

--- src/main/java/au/com/voc/raceEntry/utils/DateUtils.java
+++ src/main/java/au/com/voc/raceEntry/utils/DateUtils.java
@@ -4,26 +4,38 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class DateUtils {
-    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/MM/yyyy");
-
-    public static LocalDate format(String date) {
-        return LocalDate.parse(date, formatter);
-    }
+    private static final DateTimeFormatter USER_FORMAT = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
     public static String format(LocalDate localDate) {
-        return formatter.format(localDate);
+        return localDate.format(USER_FORMAT);
     }
 
-    public static String displayDays(LocalDate startDate, int duration) {
-        String dateOutput = format(startDate);
-        if (duration > 1) {
-            startDate.plusDays(duration - 1);
-            dateOutput += " TO " + startDate;
+    public static LocalDate format(String dateTyped) {
+
+        //default, ISO_LOCAL_DATE
+        return LocalDate.parse(dateTyped, USER_FORMAT);
+    }
+
+    @Deprecated
+    public static String displayDays(LocalDate startDate, int days) {
+        days--;
+        LocalDate endDate = startDate.plusDays(days);
+        String output = DateUtils.format(startDate);
+        if (days != 1) {
+            output += " to " + DateUtils.format(endDate);
+
         }
-        return dateOutput;
+        return output;
     }
 
     public static String displayDays(LocalDate startDate, LocalDate endDate) {
-        return format(startDate) + " TO " + endDate;
+        String output = DateUtils.format(startDate);
+        if (endDate != null) {
+            output += " to " + DateUtils.format(endDate);
+
+        }
+        return output;
     }
+
+
 }
---
+++ src/main/java/au/com/voc/raceEntry/utils/LicenceExpiry.java
@@ -0,0 +1,7 @@
+package au.com.voc.raceEntry.utils;
+
+import java.time.LocalDate;
+
+public interface LicenceExpiry {
+    LocalDate getSbaLicenceExpiryDate();
+}
