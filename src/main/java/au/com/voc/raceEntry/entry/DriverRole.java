89a3510720172b4aad6b95a83ecdad5584591397
Adam Goodridge <35621992+adamgoodridge@users.noreply.github.com>
2026-06-17 22:07:25 +1000

    delete progress

--- src/main/java/au/com/voc/raceEntry/entry/DriverRole.java
+++
@@ -1,5 +0,0 @@
-package au.com.voc.raceEntry.entry;
-
-public enum DriverRole {
-    HELMSMAN, CREW
-}
--- src/main/java/au/com/voc/raceEntry/entry/EntryStatus.java
+++
@@ -1,5 +0,0 @@
-package au.com.voc.raceEntry.entry;
-
-public enum EntryStatus {
-    PENDING, ACTIVE, CANCELLED
-}
